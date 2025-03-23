a4559900cb8701c4acd3fa4d99eb51e210fa1a35
Dominik <29484605+dominik-korsa@users.noreply.github.com>
2025-03-23 21:28:09 +0100

    Remove tests for non-required behaviors

--- satori/C/DominikKorsaGraphViewTest.java
+++ satori/C/DominikKorsaGraphViewTest.java
@@ -49,49 +49,4 @@ public class DominikKorsaGraphViewTest {
         assertNull(map.get(-5));
         assertEquals("[]", map.get(0).toString());
     }
-
-    /// Likely unnecessary, I was able to get an OK without passing this test
-    @Test
-    public void edge_set_contains_throws_correct_exceptions() {
-        GraphView graph = new GraphView();
-        var map = graph.asAdjacencyMap();
-
-        graph.addVertex();
-
-        var set = map.get(0);
-
-        assertThrows(NullPointerException.class, () -> set.contains(null));
-
-        //noinspection SuspiciousMethodCalls
-        assertThrows(ClassCastException.class, () -> set.contains("foo"));
-
-        assertFalse(set.contains(-5));
-        assertEquals("[]", map.get(0).toString());
-    }
-
-    /// Likely unnecessary, I was able to get an OK without passing this test
-    @Test
-    public void edge_set_remove_throws_correct_exceptions_and_returns_correct_result() {
-        GraphView graph = new GraphView();
-        var map = graph.asAdjacencyMap();
-
-        graph.addVertex();
-
-        var set = map.get(0);
-        set.add(0);
-
-        assertThrows(NullPointerException.class, () -> set.remove(null));
-
-        //noinspection SuspiciousMethodCalls
-        assertThrows(ClassCastException.class, () -> set.remove("foo"));
-
-        assertFalse(map.get(0).remove(-5));
-
-        assertTrue(set.contains(0));
-        assertTrue(map.get(0).remove(0));
-        assertFalse(set.contains(0));
-        assertFalse(map.get(0).remove(0));
-        assertFalse(set.contains(0));
-
-    }
 }
