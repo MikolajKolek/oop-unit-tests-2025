6c2abc77d42ad068feafdf3f881f0a3f2b8af638
MikolajKolek <kolek.mikolaj@gmail.com>
2025-04-12 18:04:03 +0200

    Add task D

---
+++ .tasks/satori/D/src/main/java/PopulationDensityAnalyzer.java
@@ -0,0 +1,23 @@
+/**
+ * PO 2024/25, Problem D - Gestosc Zaludnienia
+ *
+ * @author YOUR NAME
+ */
+
+import java.util.Map;
+
+class RestrictedAreaException extends RuntimeException {}
+
+class InvalidDensityException extends RuntimeException {}
+
+public class PopulationDensityAnalyzer {
+  public void updateDensity(int x, int y, int newValue) {}
+
+  public Map<Integer, Integer> getHistogram(int x1, int y1, int x2, int y2) {
+    return null;
+  }
+
+  public double getMedian(int x1, int y1, int x2, int y2) {
+    return 0;
+  }
+}
